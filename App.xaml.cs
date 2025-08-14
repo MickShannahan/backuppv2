5cc115d246154336bcaddb54783c9e759eebf5f1
mick shannahan <beasticorn@gmail.com>
2025-08-14 00:32:32 -0600

    can't get no icon to show

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using H.NotifyIcon;
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using H.NotifyIcon;
+using H.NotifyIcon.Core;
+using Windows.System;
 
 namespace backuppv2;
 
@@ -6,6 +10,7 @@ public partial class App : Application
 {
 	private readonly DirectoryWatcherService _w;
 	private readonly AppState _a;
+	private TrayIconWithContextMenu _trayIcon;
 	public App(DirectoryWatcherService watcher, AppState appState)
 	{
 		_a = appState;
@@ -13,9 +18,72 @@ public partial class App : Application
 		InitializeComponent();
 	}
 
+
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new MainPage()) { Title = "backuppv2" };
+		var window = new Window(new MainPage()) { Title = "backuppv2" };
+		window.Created += (object s, EventArgs e) =>
+		{
+			var nativeWindow = (Microsoft.UI.Xaml.Window)window.Handler.PlatformView;
+			var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+
+			var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(
+					Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd)
+			);
+
+			appWindow.Closing += (sender, args) =>
+			{
+				args.Cancel = true; // stops the app from closing
+				window.Hide();
+			};
+
+			InitTrayIcon();
+		};
+
+		return window;
 	}
+
+
+
+	private void OnTrayOpenClick(object sender, EventArgs e)
+	{
+		Console.WriteLine("Open Clicked");
+	}
+
+	private void OnTrayExitClick(object sender, EventArgs e)
+	{
+		Console.WriteLine("Exit Click");
+	}
+
+	private void InitTrayIcon()
+	{
+		// Init Icon
+		var iconPath = Path.Combine("Resources", "AppIcon", "ArbysGundam1.ico");
+		nint iconHandle = LoadIconHandle(iconPath);
+		var clickItem = new PopupMenuItem() { Text = "Open" };
+		clickItem.Click += OnTrayOpenClick;
+		PopupMenu menu = new()
+		{
+			Items = { clickItem }
+		};
+
+		_trayIcon = new TrayIconWithContextMenu
+		{
+			ContextMenu = menu,
+			ToolTip = "Back Up",
+			Icon = iconHandle,
+			Visibility = IconVisibility.Visible
+		};
+
+		_trayIcon.Show();
+	}
+
+	private static nint LoadIconHandle(string iconPath)
+	{
+		return LoadImage(IntPtr.Zero, iconPath, 1, 0, 0, 0x00000010);
+	}
+
+	[DllImport("user32.dll", SetLastError = true)]
+	private static extern nint LoadImage(IntPtr hInst, string lpszName, uint uType, int cx, int cy, uint fuLoad);
 
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using H.NotifyIcon;
 using CommunityToolkit.Maui;
+using Microsoft.Maui.LifecycleEvents;
+using WinRT.Interop;
+using Microsoft.UI;
 
 namespace backuppv2;
 
@@ -45,6 +48,36 @@ public static class MauiProgram
 		var azureSettings = new AzureSettings();
 		config.GetSection("Azure").Bind(azureSettings);
 		builder.Services.AddSingleton(azureSettings);
+
+		// Intercept windows close
+		// #if WINDOWS
+		// 		builder.ConfigureLifecycleEvents(events =>
+		// 		{
+		// 			events.AddWindows(windows =>
+		// 	{
+		// 		windows.OnWindowCreated((window) =>
+		// 								{
+		// 									// Hook close event
+		// 									var hwnd = WindowNative.GetWindowHandle(window);
+		// 									var winId = Win32Interop.GetWindowIdFromWindow(hwnd);
+		// 									var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(winId);
+
+		// 									appWindow.Closing += (sender, args) =>
+		// 													{
+		// 														// Cancel close
+		// 														args.Cancel = true;
+
+		// 														// Hide instead
+		// 														MainThread.BeginInvokeOnMainThread(() =>
+		// 																					{
+		// 																						// Application.Current.Windows[0].Hide();
+		// 																						Application.Current.Quit();
+		// 																					});
+		// 													};
+		// 								});
+		// 	});
+		// 		});
+		// #endif
 
 
 #if DEBUG
